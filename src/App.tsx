932f336edd4dd0d16f0a5edcc98687ae932d6650
Jonatas Brandão <jonbranaraujo18@gmail.com>
2020-10-13 14:06:43 -0300

    routes and map page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
 import React from 'react';
 
 import './styles/global.css';
-import './styles/pages/landing.css';
 
-import logoImg from './images/logo.svg';
-import enterApp from './images/logo_card.svg';
+import Routes from './routes';
 
 function App() {
-  return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy" />
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
-
-        <div className="location">
-          <strong>São Paulo</strong>
-          <span>São Paulo</span>
-        </div>
-
-        <a href={enterApp} className="enter-app">
-          >
-        </a>
-      </div>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
