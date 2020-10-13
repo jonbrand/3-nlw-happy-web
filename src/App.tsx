a04bd6b5b3087cc452876d0a9590b9928562ae9f
Jonatas Brandão <jonbranaraujo18@gmail.com>
2020-10-13 11:20:28 -0300

    home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoImg from './images/logo.svg';
+import enterApp from './images/logo_card.svg';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello NLW</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>São Paulo</strong>
+          <span>São Paulo</span>
+        </div>
+
+        <a href={enterApp} className="enter-app">
+          >
+        </a>
+      </div>
     </div>
   );
 }
